3cdc5a251b485fbf56ae46c893b88b04749469d7
Jessica Kerr <jessitron@gmail.com>
2017-02-14 12:23:08 -0600

    utility method for path expressions

--- src/test/scala/com/atomist/rug/kind/LanguageExtensionTest.scala
+++ src/test/scala/com/atomist/rug/kind/LanguageExtensionTest.scala
@@ -4,6 +4,8 @@ import _root_.java.io.File
 
 import com.atomist.rug.kind.core.ProjectMutableView
 import com.atomist.source.file.{FileSystemArtifactSource, FileSystemArtifactSourceIdentifier}
+import com.atomist.tree.UpdatableTreeNode
+import com.atomist.tree.pathexpression.{PathExpressionEngine, PathExpressionParser}
 
 /**
   * Utilities for testing a Rug language expression
@@ -12,6 +14,17 @@ trait LanguageExtensionTest {
 
   def projectFromDirectory(path: String): ProjectMutableView =
     new ProjectMutableView(FileSystemArtifactSource(FileSystemArtifactSourceIdentifier(new File(path))))
+
+  val pxe = new PathExpressionEngine()
+  val parser = PathExpressionParser
+
+  def evaluatePathExpression(project: ProjectMutableView, expression: String): Seq[UpdatableTreeNode] = {
+    val parsed = parser.parseString(expression)
+    pxe.evaluate(project, parsed) match {
+      case Left(error) => throw new RuntimeException(s"Could not parse path expression $expression: $error")
+      case Right(matches) => matches.map(_.asInstanceOf[UpdatableTreeNode])
+    }
+  }
 
 }
 
