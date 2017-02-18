3f2f59a2e9fc8059dc19758a1c45f88d1822fd62
Clay McCoy <clay@atomist.com>
2017-02-17 23:11:23 -0600

    Initial attempt at executing rugs

--- src/main/scala/com/atomist/rug/runtime/execution/LocalInstructionRunner.scala
+++ src/main/scala/com/atomist/rug/runtime/execution/LocalInstructionRunner.scala
@@ -1,13 +1,16 @@
 package com.atomist.rug.runtime.execution
 
 import com.atomist.param.SimpleParameterValues
-import com.atomist.project.edit.ProjectEditor
+import com.atomist.project.edit.{ProjectEditor, ProjectEditorUtils}
 import com.atomist.project.generate.ProjectGenerator
-import com.atomist.rug.runtime.AddressableRug
-import com.atomist.rug.runtime.plans.InstructionRunner
+import com.atomist.project.review.ProjectReviewer
+import com.atomist.rug.runtime._
+import com.atomist.rug.runtime.js.interop.jsPathExpressionEngine
+import com.atomist.rug.runtime.plans.{InstructionRunner, LocalPlanRunner, PlanRunner}
 import com.atomist.rug.spi.Handlers.Instruction._
+import com.atomist.rug.spi.Handlers.Status.{Failure, Success}
 import com.atomist.rug.spi.Handlers.{Instruction, Response}
-import com.atomist.source.ArtifactSource
+import com.atomist.source.{ArtifactSource, ArtifactSourceLocator}
 
 /**
   * Run instructions synchronously in this JVM
@@ -16,59 +19,100 @@ import com.atomist.source.ArtifactSource
   * and don't have different GA's
   */
 class LocalInstructionRunner(rugs: Seq[AddressableRug],
-                            projectFinder: ProjectFinder,
-                            projectPersister: ProjectPersister)
+                             teamIdToRunWith: String,
+                             projectFinder: ProjectFinder,
+                             projectPersister: ProjectPersister,
+                             planRunner: PlanRunner,
+                             runningPathExpressionEngine: jsPathExpressionEngine)
   extends InstructionRunner {
-  override def run(instruction: Instruction, callbackInput: Object): Response = {
-    instruction match {
-      case editor: Edit => {
-        rugs.find(p => matches(p, editor.detail)) match {
-          case rug: ProjectEditor => rug.modify(projectFinder.findArtifactSource(instruction.detail.projectName.get), SimpleParameterValues(editor.detail.parameters))
-          case _ => ???
-        }
-      }
 
-      case editor: Generate => {
-        val newproject = rugs.find(p => matches(p, editor.detail)) match {
-          case rug: ProjectGenerator => rug.generate(editor.detail.projectName.get, SimpleParameterValues(editor.detail.parameters))
-          case _ => ???
-        }
-        projectPersister.persist(editor.detail.projectName.get,newproject)
-      }
-        //TODO review - just like above. Put results in Response for a potential response handler
-        //TODO execute - look up a JVM object from a registry somewhere, run it, and return the result. Use JVM ServiceLocator stuff. Just like a Rug, but in pure JVM.
-
-        //TODO command - look up a Rug just like above (CommandHandler)
-
-        //TODO respond - also a Rug (CommandHandler)
-
-      case _ =>
+  private def doWithProjectName(instruction: Instruction, action: (String) => Response) = {
+    instruction.detail.projectName match {
+      case Some(projectName) => action(projectName)
+      case _ => Response(Failure, None, None, Some(s"Project name required for $instruction."))
     }
-    Response(null,None,None,None)
   }
 
-  /**
-    * //TODO change to find matching Rug and validate type!
-    *
-    * Does the rug match the instruction?
-    * @param rug
-    * @param detail
-    * @return
-    */
-  private def matches(rug: AddressableRug, detail: Detail): Boolean = {
-    val nameMatches = detail.name == rug.name
-    if(detail.coordinates.nonEmpty){
-      val coords = detail.coordinates.get
-      nameMatches && rug.group == coords.group && rug.name == coords.artifact
-    }else{
-      nameMatches
+  private def doWithProject(instruction: Instruction, action: (ArtifactSource) => Response) = {
+    doWithProjectName(
+      instruction,
+      (projectName: String) => {
+        projectFinder.findArtifactSource(projectName) match {
+          case Some(project) => action(project)
+          case _ => Response(Failure, None, None, Some(s"Project '$projectName' could not be found."))
+        }
+      }
+    )
+  }
+
+  override def run(instruction: Instruction, callbackInput: AnyRef): Response = {
+    val parameters = SimpleParameterValues(instruction.detail.parameters)
+    instruction match {
+      case Execute(detail) =>
+        //TODO execute - look up a JVM object from a registry somewhere, run it, and return the result. Use JVM ServiceLocator stuff. Just like a Rug, but in pure JVM.
+        Response(Success, None, None, None)
+      case _ =>
+        rugs.find(p => matches(p, instruction)) match {
+          case Some(rug: ProjectGenerator) =>
+            doWithProjectName(instruction, (projectName: String) => {
+              val newProject = rug.generate(projectName, parameters)
+              val persistAttempt = projectPersister.persist(projectName, newProject)
+              Response(Success, None, None, Some(persistAttempt))
+            })
+          case Some(rug: ProjectEditor) =>
+            doWithProject(instruction, (project: ArtifactSource) => {
+              val modificationAttempt = rug.modify(project, parameters)
+              Response(Success, None, None, Some(modificationAttempt))
+            })
+          case Some(rug: ProjectReviewer) =>
+            doWithProject(instruction, (project: ArtifactSource) => {
+              val reviewResult = rug.review(project, parameters)
+              Response(Success, None, None, Some(reviewResult))
+            })
+          case Some(rug: CommandHandler) =>
+            object context extends CommandContext{
+              override def teamId: String = teamIdToRunWith
+              override def pathExpressionEngine = runningPathExpressionEngine
+            }
+            val planOption = rug.handle(context, parameters)
+            val planResult = planOption.map(planRunner.run(_, callbackInput))
+            Response(Success, None, None, Some(planResult))
+
+          case Some(rug: ResponseHandler) =>
+            callbackInput match {
+              case response: InstructionResponse =>
+                val planOption = rug.handle(response, parameters)
+                val planResult = planOption.map(planRunner.run(_, callbackInput))
+                Response(Success, None, None, Some(planResult))
+              case _ =>
+                Response(Failure, None, None, Some("Callback input was not recognized."))
+            }
+          case rug => Response(Failure, None, None, Some(s"Cannot execute rug $rug."))
+        }
     }
+  }
+
+  private def matches(rug: AddressableRug, instruction: Instruction): Boolean = {
+    val typeMatches = (instruction, rug) match {
+      case (i: Edit, r: ProjectEditor) => true
+      case (i: Generate, r: ProjectGenerator) => true
+      case (i: Review, r: ProjectReviewer) => true
+      case (i: Command, r: CommandHandler) => true
+      case (i: Respond, r: ResponseHandler) => true
+      case _ => false
+    }
+    typeMatches && instruction.detail.name == rug.name &&
+      instruction.detail.coordinates.exists(coords => rug.group == coords.group && rug.artifact == coords.artifact)
   }
 }
 trait ProjectFinder {
-  def findArtifactSource(project_name: String) : ArtifactSource
+  def findArtifactSource(project_name: String) : Option[ArtifactSource]
 }
 
-trait ProjectPersister{
-  def persist(projectName: String, source: ArtifactSource)
+trait ProjectPersister {
+  def persist(projectName: String, source: ArtifactSource): PersistAttempt
 }
+
+sealed trait PersistAttempt
+case class FailedToPersist(t: Throwable)
+case class PersistSuccess(artifactSourceLocator: ArtifactSourceLocator)
